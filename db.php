534d21dc7dd3a31d0c67a7ceba1acd1f67d0d919
ChiaYEHHH <hedy821216@gmail.com>
2024-05-30 16:23:09 +0800

    完善function array2sql(精簡foreach迴圈) 整合(insert函式 update函式)為save函式

--- db.php
+++ db.php
@@ -37,6 +37,39 @@ function find($table, $arg)
     return $row;
 }
 
+function save($table, $array)
+{
+    //宣告全域變數
+    global $pdo;
+    // 判斷有無`id`有的話是 UPDATE 沒有的話是 INSERT
+    if (isset($array['id'])) {
+        //update
+        // update($table, $array, $array['id']);
+        //建立SQL語法
+        $sql = "UPDATE `{$table}` SET ";
+
+        //使用迴圈將欄位名稱和值組合成字串
+        $tmp = array2sql($array);
+        $sql .= join(",", $tmp);
+        
+        $sql .= " WHERE `id`='{$array}'";
+        
+        echo $sql;
+        return $pdo->exec($sql);
+    } else {
+        //insert
+        $sql = "INSERT INTO `{$table}` ";
+
+    $sql .= "(`" . join("`,`", array_keys($array)) . "`)";
+
+    $sql .= " VALUES('" . join("','", $array) . "')";
+
+    //echo $sql;
+
+    return $pdo->exec($sql);
+    }
+}
+
 function update($table, $cols, $arg)
 {
     global $pdo;
@@ -60,6 +93,21 @@ function update($table, $cols, $arg)
     return $pdo->exec($sql);
 }
 
+function insert($table, $cols)
+{
+    global $pdo;
+
+    $sql = "INSERT INTO `{$table}` ";
+
+    $sql .= "(`" . join("`,`", array_keys($cols)) . "`)";
+
+    $sql .= " VALUES('" . join("','", $cols) . "')";
+
+    //echo $sql;
+
+    return $pdo->exec($sql);
+}
+
 function del($table, $arg)
 {
     global $pdo;
@@ -78,6 +126,22 @@ function del($table, $arg)
     return $pdo->exec($sql);
 }
 
+// foreach 精簡function
+function array2sql($array)
+{
+    foreach ($array as $key => $value) {
+        $tmp[] = "`$key`='$value'";
+    }
+
+    return $tmp;
+}
+
+// 萬用function
+function q($sql)
+{
+    global $pdo;
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
 
 /**
  * 在頁面上快速顯示陣列內容
